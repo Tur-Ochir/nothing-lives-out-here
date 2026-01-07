b927b32869cc24afc88a1bb78f77ce5c06531e1c
Tur-Ochir11 <turochir11@gmail.com>
2026-01-07 18:14:08 +0800

    eating dumpling

--- Assets/1 Scripts/Dumpling.cs
+++ Assets/1 Scripts/Dumpling.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class Dumpling : Interactable
 {
     public bool cooked;
+    public Color cookedColor;
     private MeshRenderer meshRenderer;
+    public GameObject eatenVer1;
+    public int counter = 2;
 
     protected override void Start()
     {
@@ -15,6 +18,8 @@ public class Dumpling : Interactable
     public void Cook()
     {
         cooked = true;
+        col.enabled = true;
+        meshRenderer.material.color = cookedColor;
     }
 
     public bool Eat()
@@ -26,7 +31,19 @@ public class Dumpling : Interactable
         }
         
         Debug.Log("Eaten dumpling.");
-        meshRenderer.material.color = Color.peru;
+        counter--;
+
+        if (counter < 2)
+        {
+            meshRenderer.enabled = false;
+            eatenVer1.SetActive(true);
+        }
+        else if (counter < 1)
+        {
+            Destroy(gameObject);
+            PlayerManager.Instance.heldItem = null;
+        }
+        
         return true;
     }
 }
